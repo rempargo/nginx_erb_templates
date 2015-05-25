b42b3b1cc467c24270bcc21c4efb4b27c043f622
Paul Verschoor (on the server) <paul.verschoor@gmail.com>
2015-05-26 00:27:36 +0200

    create ruby script to generate nginx conf

---
+++ ncg.rb
@@ -0,0 +1,16 @@
+require 'erb'
+require './variables'
+
+server_template 	= File.read('./server_template.erb')
+upstream_template 	= File.read('./upstream_template.erb')
+
+nginx_server_conf_path		= "/etc/nginx/sites-enabled/#{@hostname}"
+nginx_upstream_conf_path	= "/etc/nginx/sites-enabled/#{@app_name}.upstream"
+
+renderer = ERB.new(server_template)
+output = renderer.result(binding)
+File.open(nginx_server_conf_path, 'w') {|f| f.write(output) }
+
+renderer = ERB.new(upstream_template)
+output = renderer.result(binding)
+File.open(nginx_upstream_conf_path, 'w') {|f| f.write(output) }
